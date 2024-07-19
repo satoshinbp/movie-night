2b7f78a3c407aea73aea909356297e43c9399ea3
shinya sato <0113shin.s@gmail.com>
2024-07-18 18:44:08 -0700

    test: use pytest

--- tests/actions/test_account.py
+++ tests/actions/test_account.py
@@ -1,45 +1,33 @@
-import unittest
-from unittest.mock import patch
 from actions.account import get_watchlist
 
 
-class TestAccount(unittest.TestCase):
-    @patch("actions.account.get_tmbd_watchlist")
-    @patch("actions.account.get_tmbd_regions")
-    @patch("actions.account.get_tmbd_provider")
-    @patch("actions.account.get_tmbd_runtime")
-    def test_get_watchlist(
-        self,
-        mock_get_tmbd_runtime,
-        mock_get_tmbd_provider,
-        mock_get_tmbd_regions,
-        mock_get_tmbd_watchlist,
-    ):
-        mock_get_tmbd_watchlist.return_value = [{"id": 1}, {"id": 2}, {"id": 3}]
-        mock_get_tmbd_regions.return_value = [
-            {"iso_3166_1": "US"},
-            {"iso_3166_1": "CA"},
-            {"iso_3166_1": "UK"},
-        ]
-        mock_get_tmbd_provider.side_effect = lambda movie_id: {
-            1: {"US": {"flatrate": [{"provider_name": "Netflix"}]}},
-            2: {"CA": {"flatrate": [{"provider_name": "Netflix"}]}},
-            3: {"UK": {"flatrate": [{"provider_name": "Amazon Prime Video"}]}},
-        }[movie_id]
-        mock_get_tmbd_runtime.side_effect = lambda movie_id: {1: 120, 2: 90, 3: 105}[
-            movie_id
-        ]
+def test_get_watchlist(mocker):
+    mock_get_tmbd_watchlist = mocker.patch("actions.account.get_tmbd_watchlist")
+    mock_get_tmbd_regions = mocker.patch("actions.account.get_tmbd_regions")
+    mock_get_tmbd_provider = mocker.patch("actions.account.get_tmbd_provider")
+    mock_get_tmbd_runtime = mocker.patch("actions.account.get_tmbd_runtime")
 
-        result = get_watchlist(123)
+    mock_get_tmbd_watchlist.return_value = [{"id": 1}, {"id": 2}, {"id": 3}]
+    mock_get_tmbd_regions.return_value = [
+        {"iso_3166_1": "US"},
+        {"iso_3166_1": "CA"},
+        {"iso_3166_1": "UK"},
+    ]
+    mock_get_tmbd_provider.side_effect = lambda movie_id: {
+        1: {"US": {"flatrate": [{"provider_name": "Netflix"}]}},
+        2: {"CA": {"flatrate": [{"provider_name": "Netflix"}]}},
+        3: {"UK": {"flatrate": [{"provider_name": "Amazon Prime Video"}]}},
+    }[movie_id]
+    mock_get_tmbd_runtime.side_effect = lambda movie_id: {1: 120, 2: 90, 3: 105}[
+        movie_id
+    ]
 
-        expected_result = [
-            {"id": 1, "netflix_regions": ["US"], "runtime": 120},
-            {"id": 2, "netflix_regions": ["CA"], "runtime": 90},
-            {"id": 3, "netflix_regions": [], "runtime": 105},
-        ]
+    result = get_watchlist(123)
 
-        self.assertEqual(result, expected_result)
+    expected_result = [
+        {"id": 1, "netflix_regions": ["US"], "runtime": 120},
+        {"id": 2, "netflix_regions": ["CA"], "runtime": 90},
+        {"id": 3, "netflix_regions": [], "runtime": 105},
+    ]
 
-
-if __name__ == "__main__":
-    unittest.main()
+    assert result == expected_result

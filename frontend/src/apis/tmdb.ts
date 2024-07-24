204ecdf3560763d7f2a4011e5cf18ac0f1a6b1b5
shinya sato <0113shin.s@gmail.com>
2024-07-24 08:38:37 -0700

    feat: add removeFromWatchlist call in client

--- frontend/src/apis/tmdb.ts
+++ frontend/src/apis/tmdb.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8000/'
+const BASE_URL = 'http://localhost:8000'
 const ACCOUNT_ID = process.env.REACT_APP_TMDB_ACCOUNT_ID
 
 export interface Movie {
@@ -24,7 +24,7 @@ export const fetchWatchlist = async (): Promise<Movie[]> => {
   if (!ACCOUNT_ID) {
     throw Error('TMDB Account ID is not set')
   }
-  const url = BASE_URL + `accounts/${parseInt(ACCOUNT_ID)}/watchlist`
+  const url = `${BASE_URL}/accounts/${parseInt(ACCOUNT_ID)}/watchlist`
   const res = await axios.get(url)
   return res.data
 }
@@ -35,7 +35,16 @@ export interface Region {
   native_name: string
 }
 export const fetchRegions = async (): Promise<Region[]> => {
-  const url = BASE_URL + 'watch/regions'
+  const url = `${BASE_URL}/watch/regions`
+  const res = await axios.get(url)
+  return res.data
+}
+
+export const removeFromWatchlist = async (movieId: number): Promise<void> => {
+  if (!ACCOUNT_ID) {
+    throw Error('TMDB Account ID is not set')
+  }
+  const url = `${BASE_URL}/accounts/${parseInt(ACCOUNT_ID)}/watchlist/remove/${movieId}`
   const res = await axios.get(url)
   return res.data
 }

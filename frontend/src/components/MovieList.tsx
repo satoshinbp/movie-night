cdfff3714803120ba439aacf6021d6fe9bb57fc9
shinya sato <0113shin.s@gmail.com>
2024-07-28 09:51:19 -0700

    feat: added link to tmdb

--- frontend/src/components/MovieList.tsx
+++ frontend/src/components/MovieList.tsx
@@ -1,4 +1,4 @@
-import { Card, CardActionArea, CardMedia, Chip, Fab, Stack, useTheme } from '@mui/material'
+import { Card, CardMedia, Chip, Fab, Stack, useTheme } from '@mui/material'
 import { Movie } from '../types/movies'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { useEffect, useRef, useState } from 'react'
@@ -29,49 +29,51 @@ export default function MovieList({
         const matchedRegions = movie.netflix_regions.filter((r) => regions.includes(r))
         return (
           <Card sx={{ position: 'relative', width }}>
-            <CardActionArea
-              href={movie.netflix_regions.length > 0 ? 'https://www.netflix.com/search?q=' + movie.title : ''}
-              target="_blank"
+            {matchedRegions.length === 0 && (
+              <div
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  backgroundColor: 'grey',
+                  opacity: 0.5,
+                }}
+                onClick={() => {
+                  window.open('https://www.themoviedb.org/movie/' + movie.id, '_blank')
+                }}
+              />
+            )}
+            <CardMedia
+              sx={{ height: width * 1.5 }}
+              image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              title={movie.title}
+              onClick={() => {
+                window.open('https://www.netflix.com/search?q=' + movie.title, '_blank')
+              }}
+            />
+            <Chip
+              label={movie.runtime + ' mins'}
+              sx={{ position: 'absolute', top: 8, left: 8 }}
+              size="small"
+              color="primary"
+            />
+            <Fab
+              size="small"
+              aria-label="remove"
+              sx={{ position: 'absolute', top: 8, right: 8 }}
+              onClick={() => onRemove(movie.id)}
             >
-              {matchedRegions.length === 0 && (
-                <div
-                  style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    backgroundColor: 'grey',
-                    opacity: 0.5,
-                  }}
-                />
-              )}
-              <CardMedia
-                sx={{ height: width * 1.5 }}
-                image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                title={movie.title}
-              />
-              <Fab
-                size="small"
-                aria-label="remove"
-                sx={{ position: 'absolute', top: 8, right: 8 }}
-                onClick={() => onRemove(movie.id)}
-              >
-                <DeleteIcon />
-              </Fab>
-              {matchedRegions.length > 0 && (
-                <Stack direction="row" spacing={1} sx={{ position: 'absolute', bottom: 8, left: 8 }}>
-                  {matchedRegions.map((r) => (
-                    <Chip key={r} label={r} color="primary" />
-                  ))}
-                </Stack>
-              )}
-              <Chip
-                label={movie.runtime + ' mins'}
-                sx={{ position: 'absolute', bottom: 8, right: 8 }}
-                color="secondary"
-              />
-            </CardActionArea>
+              <DeleteIcon />
+            </Fab>
+            {matchedRegions.length > 0 && (
+              <Stack direction="row" spacing={1} sx={{ position: 'absolute', bottom: 8, left: 8 }}>
+                {matchedRegions.map((r) => (
+                  <Chip key={r} label={r} color="primary" size="small" />
+                ))}
+              </Stack>
+            )}
           </Card>
         )
       })}
